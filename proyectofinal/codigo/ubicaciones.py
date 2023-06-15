55afe59d00fb9c85d70dd715a54650f2709dd6f8
Lautaro Larosa <lautarolarosacn56@gmail.com>
2023-06-15 03:48:30 -0300

    Agregar parametro de ubicacion movil y fija

--- proyectofinal/codigo/ubicaciones.py
+++ proyectofinal/codigo/ubicaciones.py
@@ -1,10 +1,7 @@
-
 from elementos import calculoDistanciaAutosPersonas
 
 #Función que cheque si la dirección ingresada es válida
 def chequear_direccion(mapa,direccion):
-    direccion[0][0] = direccion[0][0].lower()
-    direccion[1][0] = direccion[1][0].lower()
     if direccion[0][0] in mapa:
         x = mapa[direccion[0][0]]
         if direccion[1][0] in x:
@@ -28,6 +25,10 @@ def cargar_fija(elemento,direccion,mapa,diccionario):
         print("Ubicación ya existente")
         return
     
+    if elemento[0] == "c" or elemento[0] == "p":
+        print("Está intentando insertar una ubicación móvil")
+        return
+    
     if chequear_direccion(mapa,direccion) == True:
         diccionario[elemento] = direccion
         return diccionario
@@ -35,19 +36,23 @@ def cargar_fija(elemento,direccion,mapa,diccionario):
     return
 
 #función que carga ubicaciones móviles
-def cargar_movil(elemento,direccion,monto,mapa,diccionario):
+def cargar_movil(elemento,direccion,monto,mapa,diccionario,dicAutos):
     elemento = elemento.lower()
     if elemento in diccionario:
         print("Ubicación ya existente")
         return
     
+    if elemento[0] != "c" and elemento[0] != "p":
+        print("Está intentando insertar una ubicación fija")
+        return
+
     if chequear_direccion(mapa,direccion) == True:
         list = []
         list.append(direccion)
         list.append(monto)
         diccionario[elemento] = list
         
-        distancias = calculoDistanciaAutosPersonas(diccionario,mapa)
+        distancias = calculoDistanciaAutosPersonas(diccionario,mapa,dicAutos)
         return distancias 
     else:
         print("Dirección no válida")
